8823a3b803bc71ce721ace5c789563a54542f3f0
brandonsommer <brandon.d.sommer@gmail.com>
2017-02-11 20:29:49 -0700

    Added my individual assignment class RelocationControl.java and a test class for it 2/11/17

---
+++ RansomHacker/src/byui/cit260/ransomhacker/control/RelocationControl.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.ransomhacker.control;
+
+/**
+ *
+ * @author 50mm3r
+ */
+public class RelocationControl {
+   
+    public double calcMoveCost(int timesMoved, int newCoordX, int newCoordY, int currentCoordX, int currentCoordY) {
+        
+        int distance = (Math.abs(newCoordX - currentCoordX) + Math.abs(newCoordY - currentCoordY));
+        
+        if (distance == 0) {
+            return -1;
+        }
+        
+        double moveCost;
+        moveCost = (distance * 100) + (timesMoved * 100);
+        
+        return moveCost;
+    }
+}
