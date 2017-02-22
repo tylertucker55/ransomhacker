b8aacae8f31edd149d690c44b8427b69a709f6f5
Hansen <Hansen@Hansen>
2017-02-22 14:59:57 -0700

    I created the view layer package and removed the model layer links in the main class. it is up to date.

---
+++ RansomHacker/src/byui/cit260/ransomhacker/view/StartProgramView.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.ransomhacker.view;
+
+/**
+ *
+ * @author Hansen
+ */
+public class StartProgramView {
+    
+    private String promptMessage;
+    public StartProgramView (){
+        this.promptMessage = "\nPlease enter your name";
+        this.displayBanner();
+    }
+
+    private void displayBanner() {
+        System.out.println(
+        "\n****************************************************************************************************"
+        +"\n*                                                                                                  *"
+        +"\n*                          This is your author Hansen Muhile                                       *"
+        +"\n*                                                                                                  *"
+        +"\n*                        This is a great game. Hope you like it                                    *"
+        +"\n*                                                                                                  *"
+        +"\n*     The game is a text-based role playing game in which you take the role of a computer hacker.  *"
+        +"\n* Just after winning a cyber security competition, his one and only friend, a cat, is kidnapped by *"
+        +"\n* an unknown organization! He receives a message stating that unless he pays a hefty ransom of     *"
+        +"\n* $10,000,000.00 within 90 days, he will never see his beloved friend again.                       *"
+        +"\n*                                                                                                  *"
+        +"\n*                                                                                                  *"
+        +"\n* Being a cyber security major at his local college, he knows that a good hacker can make a lot of *"
+        +"\n* money quick and decides the only way to get the huge ransom together in time to save his         *"
+        +"\n* friend is by turning to a life of cybercrime.                                                    *"
+        +"\n*                                                                                                  *"
+        +"\n*                                                                                                  *"
+        +"\n*     The game begins in his home town of Rexburg, ID. During the game you will develop your       *"
+        +"\n* hacking skills by studying attack methods, taking courses, getting infosec certifications which  *"
+        +"\n* you will then use to carry out cyber attacks. In order to execute your attacks, you will need    *"
+        +"\n* equipment and resources, things that don’t come cheap. You work for various criminal             *"
+        +"\n* organizations, who will pay you for your skills, but they know how sneaky you hackers are and    *"
+        +"\n* insist that all payments be done in person, cash only, meaning you will have to live wherever    *"
+        +"\n* the attack takes place. The United States government also doesn’t take too kindly to people who  *"
+        +"\n* make a career of cybercrime, so you will need to relocate frequently to avoid being caught by    *"
+        +"\n* the FBI… but with new locations come new opportunities. The game ends when the allotted         *"
+        +"\n* time has passed, you get caught by the FBI or when you have enough money to pay off the          *"
+        +"\n* ransom.                                                                                          *"
+        +"\n*                                                                                                  *"
+        +"\n*                                                                                                  *"
+        +"\n****************************************************************************************************"
+        );
+    }
+
+    public void displayStartProgramView() {
+       System.out.println("\n*** displayStartProgram() function called ***");
+    }
+    
+}
--- RansomHacker/src/ransomhacker/RansomHacker.java
+++ RansomHacker/src/ransomhacker/RansomHacker.java
@@ -5,17 +5,7 @@
  */
 package ransomhacker;
 
-import byui.cit260.ransomhacker.model.Item;
-import byui.cit260.ransomhacker.model.Character;
-import byui.cit260.ransomhacker.model.Game;
-import byui.cit260.ransomhacker.model.Player;
-import byui.cit260.ransomhacker.model.Skills;
-import byui.cit260.ransomhacker.model.Attack;
-import byui.cit260.ransomhacker.model.Job;
-import byui.cit260.ransomhacker.model.Location;
-import byui.cit260.ransomhacker.model.Map;
-import byui.cit260.ransomhacker.model.Scene;
-import byui.cit260.ransomhacker.model.SkillDevelopment;
+import byui.cit260.ransomhacker.view.StartProgramView;
 
 
        
@@ -35,89 +25,10 @@ public class RansomHacker {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        StartProgramView startProgramView = new StartProgramView();
+        startProgramView.displayStartProgramView();
      
-     Item item1 = new Item();
-     item1.setName("Computer");
-     item1.setDescription("A device used to hack");
-     item1.setEffects("Required to perform basic attacks");
-     item1.setCost(500);
-     item1.setQuantity(3);
-     item1.setValue(3);
-     System.out.println(item1.toString());
-     
-     
-    
-     
-     Character character1 = new Character();
-     character1.setCoordinateX(1);
-     character1.setCoordinateY(1);
-     character1.setDaysLeft(90);
-     character1.setDetection(0);
-     character1.setHackingLevel(7);
-     character1.setItems("Computer, Laptop");
-     character1.setMoney(1000);
-     character1.setMoneyLeft(10000000);
-     character1.setName("Joe");
-     character1.setSocialLevel(5);
-     character1.setStealthLevel(3);
-     System.out.println(character1.toString());
-     
-     Game game1 = new Game();
-     game1.setDaysLeft(90);
-     System.out.println(game1.toString());
-     
-     Player player1 = new Player();
-     player1.setBestTime(78);
-     player1.setName("John");
-     System.out.println(player1.toString());
-     
-     Skills skill1 = new Skills();
-     skill1.setDescription("Used to increase your stealthiness");
-     skill1.setName("Stealth");
-     skill1.setValue(3);
-     System.out.println(skill1.toString());
-     
-     Attack attack1 = new Attack();
-     attack1.setJob("Hack Credit Card Info");
-     attack1.setChanceOfSuccess(60);
-     attack1.setDetectionCost(30);
-     attack1.setName("Brute Force Attack");
-     attack1.setTimeCost(3);
-     attack1.setSkillRequired(5);
-     System.out.println(attack1.toString());
-     
-     Job job1 = new Job();
-     job1.setName("Hack Credit Card Info");
-     job1.setPay(10000);
-     job1.setState("Idaho");
-     System.out.println(job1.toString());
-     
-     Location location1 = new Location();
-     location1.setColumn(1);
-     location1.setRow(1);
-     location1.setVisited(true);
-     System.out.println(location1.toString());
-     
-     Map map1 = new Map();
-     map1.setColumnCount(100);
-     map1.setRowCount(100);
-     System.out.println(map1.toString());
-     
-     Scene scene1 = new Scene();
-     scene1.setDisplaySymbol("$");
-     scene1.setName("State");
-     scene1.setTravelTime(10);
-     System.out.println(scene1.toString());
-     
-     SkillDevelopment skilldevelopment1 = new SkillDevelopment();
-     skilldevelopment1.setName("Self-Study");
-     skilldevelopment1.setMoneyCost(100);
-     skilldevelopment1.setState("Idaho");
-     skilldevelopment1.setTimeCost(1);
-     skilldevelopment1.setValue(1);
-     skilldevelopment1.setDescription("Study Hacking Strategies in a day long Personal Study");
-    
-
     }
     
     
