482394fb99d6ba4b25d6c6fe58bfaeef9a212490
Hansen <Hansen@Hansen>
2017-03-07 13:29:51 -0700

    i fixed the compiling error

--- RansomHacker/src/byui/cit260/ransomhacker/control/GameControl.java
+++ RansomHacker/src/byui/cit260/ransomhacker/control/GameControl.java
@@ -40,8 +40,9 @@ public class GameControl {
         System.out.println("\n*** createNewGame stub function called***");
     }
 
-    public static void LoadGame(String input) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static boolean LoadGame(String input) {
+        System.out.println("\n*** loadGame stub function called***");
+        return true;
     }
     
 }
